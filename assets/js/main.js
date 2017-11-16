64733f7cbfd7c5569ac0a5eeb5f323fc423bfd94
Priyansh Rastogi <priyanshrastogi54@gmail.com>
2017-11-16 22:01:54 +0530

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,77 @@
+/*
+ * Change Navbar color while scrolling
+*/
+$(document).ready(function(){
+$(window).scroll(function(){
+    $(".slideanim").each(function(){
+      var pos = $(this).offset().top;
+
+      var winTop = $(window).scrollTop();
+        if (pos < winTop + 600) {
+          $(this).addClass("slide");
+        }
+    });
+	handleTopNavAnimation();
+});
+});
+
+function handleTopNavAnimation() {
+	var top=$(window).scrollTop();
+
+	if(top>10){
+		$('#site-nav').addClass('navbar-solid'); 
+	}
+	else{
+		$('#site-nav').removeClass('navbar-solid'); 
+	}
+}
+
+/*
+ * Registration Form
+*/
+
+$('#registration-form').submit(function(e){
+    e.preventDefault();
+    
+    var postForm = { //Fetch form data
+            'fname'     : $('#registration-form #fname').val(),
+            'lname'     : $('#registration-form #lname').val(),
+            'email'     : $('#registration-form #email').val(),
+            'cell'      : $('#registration-form #cell').val(),
+            'address'   : $('#registration-form #address').val(),
+            'zip'       : $('#registration-form #zip').val(),
+            'city'      : $('#registration-form #city').val(),
+            'program'   : $('#registration-form #program').val()
+    };
+
+    $.ajax({
+            type      : 'POST',
+            url       : './assets/php/contact.php',
+            data      : postForm,
+            dataType  : 'json',
+            success   : function(data) {
+                            if (data.success) {
+                                $('#registration-msg .alert').html("Registration Successful");
+                                $('#registration-msg .alert').removeClass("alert-danger");
+                                $('#registration-msg .alert').addClass("alert-success");
+                                $('#registration-msg').show();
+                            }
+                            else
+                            {
+                                $('#registration-msg .alert').html("Registration Failed");
+                                $('#registration-msg .alert').removeClass("alert-success");
+                                $('#registration-msg .alert').addClass("alert-danger");
+                                $('#registration-msg').show();
+                            }
+                        }
+        });
+});
+
+$('.navbar-nav>li>a').not("#aboutlink").on('click', function(){
+    $('.navbar-collapse').collapse('hide');
+});
+/*
+ * SmoothScroll
+*/
+
+smoothScroll.init();
